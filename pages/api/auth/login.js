3006bc70a87052517dc733ac0d47bdc93dca0f07
Zainul Muhaimin <45698305+znmn@users.noreply.github.com>
2023-04-04 22:52:04 +0700

    feat: add bcrypt dependency to package.json feat: add authMiddleware function to _middleware.js feat: add login API endpoint to pages/api/auth/login.js feat: add register API endpoint to pages/api/auth/register.js feat: add table models to prisma/schema.prisma

---
+++ pages/api/auth/login.js
@@ -0,0 +1,44 @@
+import bcrypt from "bcrypt";
+import prisma from "@/lib/_prisma";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET,
+	JWT_EXPIRY = process.env.JWT_EXPIRY;
+
+export default async function handle(req, res) {
+	let success = false;
+	try {
+		switch (req.method) {
+			case "POST":
+				const { email, password } = req.body;
+				if (!email || !password) {
+					return res.status(400).json({ success, message: "Email and password are required" });
+				}
+
+				const admin = await prisma.staff.findUnique({ where: { email } });
+				if (!admin) {
+					return res.status(400).json({ success, message: "Admin with this email does not exist" });
+				}
+
+				console.log(await bcrypt.hash(admin.password, 10), admin.password);
+				const isPasswordValid = await bcrypt.compare(password, admin.password);
+				if (!isPasswordValid) {
+					return res.status(400).json({ success, message: "Invalid password" });
+				}
+
+				const token = jwt.sign({ userId: admin.id }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
+
+				res.setHeader("Authorization", `Bearer ${token}`);
+				res.setHeader("Set-Cookie", `token=${token}; Path=/; Max-Age=86400`);
+				return res.status(200).json({
+					success: true,
+					token,
+				});
+			default:
+				return res.status(405).json({ success, message: "Method not allowed" });
+		}
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ success, message: "Internal server error" });
+	}
+}
---
+++ pages/api/auth/register.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+import prisma from "@/lib/_prisma";
+
+export default async function handle(req, res) {
+	let success = false;
+	try {
+		switch (req.method) {
+			case "POST":
+				let { nama_awal, nama_akhir, no_telp, tanggal_lahir, alamat, kota, email, password } = req.body;
+
+				if (!nama_awal || !nama_akhir || !no_telp || !tanggal_lahir || !alamat || !kota || !email || !password) {
+					return res.status(400).json({ success, message: "nama_awal, nama_akhir, no_telp, tanggal_lahir, alamat, kota, email, and password field are required" });
+				}
+
+				const isExist = await prisma.staff.findUnique({ where: { email } });
+				if (isExist) {
+					return res.status(400).json({ success, message: "Admin with this email already exist" });
+				}
+
+				const hashedPassword = await bcrypt.hash(password, 10);
+				const admin = await prisma.staff.create({
+					data: {
+						nama_awal,
+						nama_akhir,
+						no_telp,
+						tanggal_lahir: new Date(tanggal_lahir),
+						alamat,
+						kota,
+						email,
+						password: hashedPassword,
+					},
+				});
+
+				delete admin.password;
+
+				return res.status(201).json({ success: true, data: admin });
+			default:
+				return res.status(405).json({ success, message: "Method not allowed" });
+		}
+	} catch (err) {
+		console.error(err);
+		if (err instanceof RangeError) {
+			return res.status(400).json({ success, message: "Invalid date format" });
+		}
+		return res.status(500).json({ success, message: "Internal server error" });
+	}
+}
