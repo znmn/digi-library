ed6cf0d5bf4cbbb6d26f327627765db95323d20b
Zainul Muhaimin <45698305+znmn@users.noreply.github.com>
2023-06-18 08:44:28 +0700

    Update

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,13 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<style>
+					{`
+						nextjs-portal {
+							display: none;
+						}
+					`}
+				</style>
 				<link rel="shortcut icon" href="/favicon.png" type="image/png" />
 			</Head>
 			<body>
